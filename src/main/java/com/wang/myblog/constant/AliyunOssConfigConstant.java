e58cbd000375ac7db277b318d95846810aa7414b
LUKE-bit <57066589+LUKE-bit@users.noreply.github.com>
2022-07-20 17:37:44 +0800

    Update AliyunOssConfigConstant.java

--- src/main/java/com/wang/myblog/constant/AliyunOssConfigConstant.java
+++ src/main/java/com/wang/myblog/constant/AliyunOssConfigConstant.java
@@ -7,11 +7,11 @@ public class AliyunOssConfigConstant {
     //仓库名称
     public static final String BUCKE_NAME = "mywblog";
     //地域节点
-    public static final String END_POINT = "oss-cn-beijing.aliyuncs.com";
+    public static final String END_POINT = 
     //AccessKey ID
-    public static final String ACCESSKEY_ID = "LTAI4GJPBaozWDehpXDBWs9r";
+    public static final String ACCESSKEY_ID = 
     //Access Key Secret
-    public static final String ACCESSKEY_SECRET = "yRngeYkOaGR4MKyo7vQaC09eDWmlJZ";
+    public static final String ACCESSKEY_SECRET = 
     //仓库中的文件夹
     public static final String FILE_HOST = "blog";
 }
